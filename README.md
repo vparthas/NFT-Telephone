aa1ff2715211e82c7538f79f68d5cbd22a8c865c
Varun P <varunp@Varuns-MacBook-Pro.local>
2022-01-25 17:15:01 -0600

    Add latest contract addresses

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # NFT Telephone
 *This project is a work in progress.*
+- Ropsten address (latest version): 0xd7d73bD9ef35911eB713e768BcfB79a19F905093
 
 A solidity smart contract to allow for a decentralized game of telephone using NFTs linking to IPFS CIDs. The rules are as follows:
 - The owner of the latest token has exclusive transformation privileges (the right to specify the source of the next token) for 6500 blocks.
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,6 @@
 const hre = require("hardhat");
 
 async function main() {
-    console.log("Contract deployed to:", telephone.address);
-
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
@@ -11,7 +9,7 @@ async function main() {
     const telephone = await Telephone.deploy("https://ipfs.io/ipfs/QmW3FgNGeD46kHEryFUw1ftEUqRw254WkKxYeKaouz7DJA"); //random cid
     await telephone.deployed();
 
-    console.log("contract address:", token.address);
+    console.log("contract address:", telephone.address);
   }
   
   // We recommend this pattern to be able to use async/await everywhere
