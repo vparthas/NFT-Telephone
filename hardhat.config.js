e05ae843d922e0053883d8e611960cd0cb717c47
Varun P <varunp@Varuns-MacBook-Pro.local>
2022-01-25 17:04:26 -0600

    add deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
 
+const ALCHEMY_API_KEY = "LOL";
+const ROPSTEN_PRIVATE_KEY = "YOU";
+const MAINNET_PRIVATE_KEY = "THOUGHT";
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -18,4 +22,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    },
+    mainnet: {
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${MAINNET_PRIVATE_KEY}`]
+    }
+  }
 };
